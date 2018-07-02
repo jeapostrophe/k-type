5c4821e86a7825be9ea1f4431fa5e4dada070436
portable <slavik@krut.aga>
2018-07-02 23:44:45 +0300

    vk post text

--- rtype.js
+++ rtype.js
@@ -22,14 +22,16 @@ window.sendData = function(data) {
 
 window.rtype = (function () {
     var ratingElement = document.getElementById('rating');
+    var avgSpeedContainerElement = document.getElementById('avg_speed');
+    var avgSpeedValueElement = document.getElementById('avg_speed_value');
 
     var fields = {
         'position': '#',
         'score': 'score',
         'wave': 'wave',
-        'hits': 'hits',
-        'misses': 'misses',
-        'accuracy': 'acc%'
+        'speed': 'speed',
+        'typing_accuracy': 'acc%',
+        'share': 'share'
     };
 
     var keys = Object.keys(fields);
@@ -37,15 +39,17 @@ window.rtype = (function () {
     function drawUserRating(games) {
         var tbl = document.createElement('table');
         tbl.className = 'tbl';
+
+        var speedSum = 0, speedCount = 1;
+
         for (var i = -1; i < games.length && i < 20; i++) {
             if (i >= 0) {
                 games[i]['position'] = i + 1;
                 var accuracy = games[i].hits ? games[i].hits / (games[i].hits + games[i].misses) * 100 : 0;
                 games[i]['accuracy'] = accuracy.toFixed(2);
-
             }
             var tr = document.createElement('tr');
-            keys.forEach(function(key) {
+            keys.forEach(function (key) {
                 var e;
                 if (i < 0) {
                     tr.className = 'thead';
@@ -53,15 +57,66 @@ window.rtype = (function () {
                     e.innerHTML = fields[key];
                 } else {
                     e = document.createElement('td');
-                    e.innerHTML = games[i][key];
+                    var game = games[i];
+                    if (key === 'share') {
+                        if (game['speed']) {
+                            var a = document.createElement('a');
+                            a.innerHTML = '<img class="vk_btn" src="media/vk.png">';
+                            a.href = '#';
+
+                            a.addEventListener('click', function (ev) {
+                                ev.preventDefault();
+
+                                var text = 'На ' + (game['difficulty'] == 'easy' ? 'легком' : 'сложном')
+                                    + ' уровне со скоростью ' + game['speed'].toFixed(1) + ' знаков в минуту мне удалось достичь '
+                                    + game['wave'] + ' волны. Попробуй и ты!\nhttps://typeordie.space';
+
+                                sendData({
+                                    action: 'btn_click',
+                                    btn_name: 'share_vk'
+                                });
+
+                                VK.Api.call('wall.post', {
+                                    v: '5.73',
+                                    message: text
+                                }, function (r) {
+                                    sendData({
+                                        action: 'vk_api_result',
+                                        response: r
+                                    })
+                                })
+
+                                return false;
+                            })
+                            e.appendChild(a);
+                        }
+                    } else {
+                        var value = key in games[i] ? games[i][key] : '';
+                        if (value && (key == 'speed' || key == 'typing_accuracy')) {
+                            if (key == 'speed') {
+                                speedSum += value;
+                                speedCount++;
+                            }
+                            value = value.toFixed(1);
+                        }
+                        e.innerHTML = value;
+                    }
                 }
                 tr.appendChild(e);
-            })
+            });
             tbl.appendChild(tr);
         }
+
+        avgSpeedValueElement.innerHTML = (speedSum / speedCount).toFixed(1);
+
         ratingElement.style.left = 'calc(50% + ' + (CANVAS_WIDTH / 2 + 50) + 'px)';
-        ratingElement.style.top = 'calc(50% - ' + (tbl.offsetHeight / 2 + CANVAS_HEIGTH / 2) + 'px)';
-        ratingElement.innerHTML = tbl.outerHTML;
+        avgSpeedContainerElement.style.left = ratingElement.style.left;
+        avgSpeedContainerElement.style.top = 'calc(50% - ' + (tbl.offsetHeight / 2 + CANVAS_HEIGTH / 2) + 'px)';
+        window.avgElement = avgSpeedContainerElement;
+        ratingElement.style.top = (avgSpeedContainerElement.offsetTop + avgSpeedContainerElement.offsetHeight + 10) + 'px';
+        // ratingElement.style.top = 'calc(50% - ' + (tbl.offsetHeight / 2 + CANVAS_HEIGTH / 2) + 'px)';
+        ratingElement.innerHTML = '';
+        ratingElement.appendChild(tbl);
         ratingElement.style.display = 'inline-block';
     }
 
